526e0ae37f729f283eeb9c1fa5e9ad62c4b7923f
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-27 17:35:30 +0200

    Feat: add for cyclo to mounted

--- main.js
+++ main.js
@@ -8,28 +8,21 @@ createApp ({
             // titolo da stampare in pagina
             title: "Lista di e-mail",
             // lista e-mail da generare
-            mailList: [
-                "",
-            ]
+            mailList: []
         }
     },
     // metodo per generare 10 e-mai con la funzione axios
-    methods:{
-        dieci(){
-            if(this.mailList.length < 10){
-            }
-        }
-    },
-
     // le funfioni per stampare le 10 mail random
     mounted () {
-        
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then ((mailItem) =>{
                 const eMail = mailItem.data.response;
                 console.log(eMail);
-                this.mailList= eMail;
+                this.mailList.push(eMail);
             })
+        }
+
     }
     
 }).mount('#app')
