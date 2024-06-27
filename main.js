a5d4cbcfe3472bf173749d5f74e3d25da5feb090
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-27 16:17:15 +0200

    feat: add axios for generate mails

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
+// creiamo una lista di 10 e-mail e stampiamoli in pagina
+// struttura dati Vue
 const {createApp} = Vue;
 
 createApp ({
     data () {
         return {
+            // titolo da stampare in pagina
             title: "Lista di e-mail",
+            // lista e-mail da generare
             mailList: [
                 "",
             ]
         }
     },
+
+    // le funfioni per stampare le 10 mail random
+    mounted () {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then (function(mailItem) {
+                const eMail = mailItem.data;
+                console.log(eMail);
+            })
+    }
     
 }).mount('#app')
 
